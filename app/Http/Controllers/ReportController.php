c466607ef3470bff14144e4cf18e530adb9f03e4
Diana Chacon <dchacon871@unab.edu.co>
2021-06-09 21:10:57 -0500

    Procesamiento de la tabla temporal

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -22,6 +22,15 @@ class ReportController extends Controller
         $to = $request->get('date_finish');
         $period = new CarbonPeriod($from, $to);
 
+
+        DB::statement("DELETE from temporal");
+        DB::statement("INSERT INTO temporal(day_temporal, total_sale_cash, total_sale_baucher) (SELECT day_sale_cash, total_sale_cash, total_sale_baucher FROM total_sales_view WHERE day_sale_cash BETWEEN '$from' AND '$to')");
+        DB::statement("UPDATE temporal SET total_sales = (IFNULL(total_sale_cash,0)+ IFNULL(total_sale_baucher,0))");
+        DB::statement("UPDATE temporal JOIN total_expenses_view ON temporal.day_temporal = total_expenses_view.day_expense_cash SET temporal.total_expense_cash = total_expenses_view.total_expense_cash");
+        DB::statement("UPDATE temporal JOIN total_expenses_view ON temporal.day_temporal = total_expenses_view.day_expense_baucher SET temporal.total_expense_baucher = total_expenses_view.total_expense_baucher");
+        DB::statement("UPDATE temporal SET total_expenses = (IFNULL(total_expense_cash,0)+ IFNULL(total_expense_baucher,0))");
+        DB::statement("UPDATE temporal SET total_cash_day = (IFNULL(total_sales,0)+ IFNULL(total_expenses,0))");
+
         $total_sale_cash = DB::table('sales_cash_view')
         ->select('day_sale_cash', 'total_sale_cash')
         ->whereBetween('day_sale_cash', [$from, $to])
@@ -31,6 +40,8 @@ class ReportController extends Controller
         ->select('day_sale_baucher', 'total_sale_baucher')
         ->whereBetween('day_sale_baucher', [$from, $to])
         ->get();
+
+
                                 
         foreach($period as $day){
             foreach($total_sale_cash as $total){
@@ -50,11 +61,12 @@ class ReportController extends Controller
         $total_baucher = collect($total_sale_baucher)->sum('total_sale_baucher');
         $total_sales = $total_cash + $total_baucher;
 
-        return $total_cash;
+        $pdf   = PDF::loadView('pdf.report', compact('period', 'total_sale_cash','total_sale_baucher', 'total_cash', 'total_baucher', 'total_sales'));
+        return $pdf->stream('report.pdf');  
 
     }
 
-    public function exportPdf()
+    /**public function exportPdf($from, $to)
     {
         
         //$from = '2021-06-03 00:00:00';
@@ -91,5 +103,5 @@ class ReportController extends Controller
 
         $pdf   = PDF::loadView('pdf.report', compact('period', 'total_sale_cash','total_sale_baucher', 'total_cash', 'total_baucher', 'total_sales'));
         return $pdf->stream('report.pdf');
-    }
+    }**/
 }
