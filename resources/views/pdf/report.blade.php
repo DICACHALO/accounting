70e1c17884c252f434c1c2d3bbeff447ffa308ed
Diana Chacon <dchacon871@unab.edu.co>
2021-09-25 11:34:27 -0500

    Fix pdf resume report

--- resources/views/pdf/report.blade.php
+++ resources/views/pdf/report.blade.php
@@ -2,7 +2,9 @@
 <html>
 <head>
 <style>
-
+body{
+    margin: 60px 60px 60px 40px;
+}
 .table {
   font-family: arial, sans-serif;
   border-collapse: collapse;
@@ -28,74 +30,80 @@ tr:nth-child(even) {
 
 footer {
     color: black;
-    text-align: center;
+    text-align: left;
     position: absolute;
     bottom: 0;
     width: 100%;
     height: 40px;
         }
 
+img{
+    display:static;
+margin-left: 150px;
+
+}
 </style>
 </head>
 <body>
     <div class="container">
-        
 
-<h2 align="center"> ALMACÉN INSTITUTO DE BELLEZA MARLENE</h2>
+
+<h2 align="center"> ALMACÉN INSTITUTO DE BELLEZA MARLENE</h2><br>
+<img src="https://institutomarlene.edu.co/wp-content/uploads/2021/04/cropped-cropped-LogoMarleneWeb-04.png" alt="Logo">
 <h3 align="center">Reporte cuadre de caja</h3>
 <br>
-<h4> Informe resumido del {{ $fromDate }} al {{ $toDate }}</h4>
 
+<p> Informe resumido del <strong>{{ $fromDate }}</strong> al <strong>{{ $toDate }}</strong></p>
+<br><br>
 
-    <table align="center"><caption align="center"><strong> 1.VENTAS </strong></caption>
+    <table align="center"><caption align="center"><strong> TOTAL DE VENTAS </strong></caption>
         <thead>
         <tr>
-            <th>Ventas en efectivo</th>
-            <th>Ventas con baucher</th>
+            <th>Efectivo</th>
+            <th>Baucher</th>
             <th>Total</th>
         </tr>
         </thead>
         <tbody>
             <tr>
-                <td> $ {{  number_format($total_sale_cash) }}</td>
+                <td> $ {{ number_format($total_sale_cash) }}</td>
                 <td> $ {{ number_format($total_sale_baucher) }}</td>
                 <td> $ {{ number_format($total_sales) }}</td>
-            </tr>         
+            </tr>
         <tbody>
     </table>
 
-<br>
+<br><br><br>
 
-    <table align="center"><caption align="center"><strong> 2. GASTOS </strong></caption>
+    <table align="center"><caption align="center"><strong> TOTAL DE GASTOS </strong></caption>
         <thead>
         <tr>
             <th>Efectivo</th>
             <th>Consignación</th>
-            <th>Total</th>           
+            <th>Total</th>
         </tr>
         </thead>
         <tbody>
             <tr>
                 <td> $ {{ number_format($total_expense_cash) }}</td>
                 <td> $ {{ number_format($total_expense_baucher) }}</td>
-                <td> $ {{ number_format($total_expenses) }}</td>              
+                <td> $ {{ number_format($total_expenses) }}</td>
             </tr>
-    
+
         <tbody>
     </table>
 
-<br>   
+<br>
 <p><strong>Saldo en efectivo: </strong>$ {{ number_format($total_cash_day) }}</p>
-<br>  
+<br>
 
-<p> Elabora: __________________________________ &nbsp;&nbsp;&nbsp; 
-    Recibe:  __________________________________ </p>
+<p> Elabora: _____________________________ &nbsp;&nbsp;&nbsp;
+    Recibe:  _____________________________ </p>
 <br><br><br><br>
     </div>
 
 <footer>
-    
 Fecha de impresión: {{ $today }}
-    </footer>
+</footer>
 </body>
 </html>
